5f3e1303cd9bc6f9643c7afce93f599a780f3f97
Philmg <phillipmguetterman@att.net>
2023-08-21 00:25:53 -0500

    add necessary code files

---
+++ generateLogo.js
@@ -0,0 +1,25 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderShape(shape) {
+  if (license === "Circle") {
+    return ""
+  }
+  else if (license === "Square") {
+    return ""
+  }
+  else if (license === "Triangle") {
+    return ""
+  }
+  else {
+    return "";
+  }
+}
+
+
+function generateSVG(response) {
+  return `
+
+`;
+};
+
+module.exports = { generateSVG };
---
+++ index.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./generateLogo');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Choose up to three characters for your logo.',
+        name: 'characters',
+    },
+    {
+        type: 'input',
+        message: 'What color do you want your TEXT to be?',
+        name: 'textcolor',
+    },
+    {
+        type: 'list',
+        message: 'What shape do you want?',
+        choices: ["Circle", "Square", "Triangle", "none"],
+        default: "none",
+        name: 'shape',
+    },
+    {
+        type: 'input',
+        message: 'What color do you want your SHAPE to be?',
+        name: 'shapecolor',
+    },
+]
+
+inquirer
+    .prompt(questions)
+
+    // .then((data) => {
+    //     let allData = markdown.generateMarkdown(data);
+    //     fs.writeFile('.logo.svg', allData, (err) =>
+    //         err ? console.error(err) : console.log('Generated logo.svg')
+    //     )
+    // });
