4754dee318bb2c77d6f3961751376a84812ac2a5
Philmg <phillipmguetterman@att.net>
2023-08-25 18:05:48 -0500

    create shape classes

--- classes.js
+++ classes.js
@@ -5,22 +5,40 @@ class SVG {
     }
     render() {
         return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-${this.shape} ${this.text}
+            ${this.shape} ${this.text}
         </svg>`
     }
 }
 
-class Circle {
-    constructor(color,)
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+
 }
 
-class Square {
-    constructor(color,)
+class Circle extends Shape {
+    render() {
+        return `<circle cx="150" cy="100" r="80" fill="${this.color}" />`
+    }
 }
 
-class Triangle {
-    constructor(color,)
+class Square extends Shape {
+    render() {
+        return `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`
+    }
 }
+
+class Triangle extends Shape {
+    render() {
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`
+    }
+}
+
+
+/* <rect x="90" y="40" width="120" height="120" fill="turquoise" />
+
+<polygon points="150, 18 244, 182 56, 182" fill="bisque" />
 
 <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
@@ -28,4 +46,4 @@ class Triangle {
 
     <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
 
-</svg>
+</svg> */
